133bd1105e6f079fbe98d1a078afdbda4ee69d3c
inzamulhaque <mdihalif@gmail.com>
2022-03-22 23:49:05 +0600

    first commit

---
+++ src/components/shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                products-container
+            </div>
+            <div className="cart container">
+                cart container
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
