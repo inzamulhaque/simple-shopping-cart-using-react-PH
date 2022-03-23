b0b8d4618b452c32954c1634988a8a60ff7faec8
inzamulhaque <mdihalif@gmail.com>
2022-03-23 15:16:17 +0600

    fetch data

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("products.json")
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
     return (
         <div className='shop-container'>
             <div className="products-container">
